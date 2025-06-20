5602c728c9b2e40296275857429b804a61e82fd5
markpopovich9 <betmen070809@gmail.com>
2025-06-20 22:36:04 +0300

    go

--- post_app/views.py
+++ post_app/views.py
@@ -10,7 +10,11 @@ from django.views.generic.edit import FormView
 from  django.contrib.auth.models import User
 from  django.contrib.auth.decorators import login_required
 # Create your views here.
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 @login_required(login_url=reverse_lazy('login'))
@@ -110,6 +114,18 @@ def new_posts(request:WSGIRequest):
         
         return render(request, "post_app/new_posts.html", context={'list_posts':list_posts, "type":type})
     return 'onlyPost'
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user) 
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Будь ласка, виправте помилки нижче.')
+    else:
+        form = PasswordChangeForm(request.user)
 class Posts(FormView):
     template_name = "post_app/posts.html"
     form_class = messageForm
