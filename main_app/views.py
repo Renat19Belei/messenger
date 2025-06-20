8d50dea4571a25ca0e8c30f93fc0beecb2bce8a8
Renat19Belei <resyaresya1908@gmail.com>
2025-06-20 22:09:08 +0300

    commit2

--- main_app/views.py
+++ main_app/views.py
@@ -183,6 +183,19 @@ def albums(request:WSGIRequest):
     user_albums = Album.objects.filter(user=request.user)
     
     return render(request, 'main_app/albums.html', context= {"albums" :user_albums})
+def album_detail_view(request, album_pk):
+    album = get_object_or_404(Album, pk=album_pk)
+    all_images = list(album.images.all().order_by('id')) 
+    split_point = (len(all_images) + 1) // 2
+    row1_images = all_images[:split_point]
+    row2_images = all_images[split_point:]
+
+    context = {
+        'album': album,
+        'row1_images': row1_images,
+        'row2_images': row2_images,
+    }
+    return render(request, 'main_app/albums.html', context)
 # =======
 #     return render(request, 'main_app/albums.html')
 
